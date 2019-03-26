af796cc15766f9bc32d40fc9dd63ddada85a118a
user <user@begbol>
2019-03-26 12:57:36 -0700

    5. update

--- SeleniumAutomationFrameWork/src/main/java/com/my/pages/BasePage.java
+++ SeleniumAutomationFrameWork/src/main/java/com/my/pages/BasePage.java
@@ -1,11 +1,5 @@
 package com.my.pages;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -30,15 +24,6 @@ public class BasePage {
 		driver.get("https://demo.nopcommerce.com/");
 
 		return driver;
-	}
-	public void failed() {
-		File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		try {
-			FileUtils.copyFile(srcFile, new File("/Users/user/git/FrameWork/SeleniumAutomationFrameWork/screenshots"));
-			
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
 	}
 	
 }
